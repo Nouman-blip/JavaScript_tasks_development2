2237780ab4b546d84aadee81f1d46fdb28061947
Nouman-blip <attaussmalikki@gmail.com>
2024-01-24 12:57:49 +0500

    math operations task3 completed

---
+++ math.js
@@ -0,0 +1,24 @@
+const num=25;
+
+// calculate the sqrt of num
+
+const sqrt1=Math.sqrt(num);
+
+console.log(sqrt1);
+
+
+// b. Generate a random number between 1 and 10 (inclusive).
+
+let randNum = Math.floor(Math.random() * 9) + 1;
+
+console.log(randNum);
+
+//c.  Round 3.14159 to the nearest integer.
+
+const pi=3.14159;
+const roundPi=Math.round(pi);
+
+console.log(roundPi)
+
+
+
